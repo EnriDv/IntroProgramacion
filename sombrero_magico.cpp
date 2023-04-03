d553af6e43b3796fdd53fdb6246486eae54efff0
EnriDv <129296040+EnriDv@users.noreply.github.com>
2023-04-03 10:42:20 -0400

    Create sombrero_magico.cpp

---
+++ sombrero_magico.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    /*”
+    Hace mil años o más,
+    Cuando estaba recién cosido,
+    Vivían cuatro magos de renombre,
+    Cuyos nombres aún son bien conocidos”
+    */
+    cout<<"Bienvenido, el quiz del sombrero magico comienza!! ."<<endl;
+    int slith,griff,huff,raven
+    for(int i=1;i<=4;++i)
+    {
+        
+    }
+    return 0;
+}
